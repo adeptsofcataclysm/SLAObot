b6fec6b9e99c29339d7697cb4ee04af6d749a680
drcrazy <hcb@wowhcb.ru>
2021-08-25 01:39:44 +0300

    Fetch more data and generate embed

--- slao_bot.py
+++ slao_bot.py
@@ -1,12 +1,14 @@
 import os
 import aiohttp
+import discord
+import asyncio
 
 from dotenv import load_dotenv
+from datetime import datetime, timezone
 from discord.ext import commands
 from gql import Client
 from gql.dsl import DSLQuery, DSLSchema, dsl_gql
 from gql.transport.aiohttp import AIOHTTPTransport
-
 
 load_dotenv()
 bot = commands.Bot(command_prefix='!')
@@ -19,6 +21,8 @@ async def on_ready():
 
 @bot.command(name='wcl', help='Get data from report. Format: !wcl SOME_REPORT_ID')
 async def get_data(ctx, report_id):
+    async with ctx.typing():
+        await asyncio.sleep(5)
 
     async with aiohttp.ClientSession() as cs:
         data = {'grant_type': 'client_credentials'}
@@ -39,6 +43,7 @@ async def get_data(ctx, report_id):
                     ds.ReportData.report(code=report_id).select(
                         ds.Report.startTime,
                         ds.Report.endTime,
+                        ds.Report.owner.select(ds.User.name),
                         ds.Report.exportedSegments,
                         ds.Report.zone.select(ds.Zone.name)
                     ))
@@ -46,8 +51,22 @@ async def get_data(ctx, report_id):
                 query = dsl_gql(DSLQuery(query_report))
                 result = await cl.execute(query)
 
-    response = result['reportData']['report']['zone']['name']
-    await ctx.send(response)
+    report_title = result['reportData']['report']['zone']['name']
+    report_owner = result['reportData']['report']['owner']['name']
+    report_start = make_time(result['reportData']['report']['startTime'])
+    report_end = make_time(result['reportData']['report']['endTime'])
+
+    embed = discord.Embed(title=report_title, description=f"Reported by {report_owner}", color=0x6b6b6b)
+    embed.add_field(name="Start time", value=report_start, inline=True)
+    embed.add_field(name="End time", value=report_end, inline=True)
+
+    await ctx.send(embed=embed)
+
+
+def make_time(timestamp: int):
+    utc_time = datetime.fromtimestamp(float(timestamp)/1000, timezone.utc)
+    local_time = utc_time.astimezone()
+    return local_time.strftime("%Y-%m-%d %H:%M (%Z)")
 
 
 bot.run(os.getenv('DISCORD_TOKEN'))
