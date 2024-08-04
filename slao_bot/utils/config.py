689b751d016d8828bb691d3cedde7d58a17d7c1b
drcrazy <hcb@wowhcb.ru>
2024-08-04 04:17:11 +0300

    Fix flake

--- slao_bot/utils/config.py
+++ slao_bot/utils/config.py
@@ -7,9 +7,9 @@ guild_config = configparser.ConfigParser()
 guild_config.read('config/guild.cfg')
 
 default_config = {
-                    'SIGNUP_ENABLED': False,
-                    'SIGNUP_CHANNEL': 0,
-                    'WELCOME_MESSAGE': 'Welcome to guild server. Use /signup command if you want to join our guild!',
-                    'EPGP_ENABLED': False,
-                    'EPGP_UPLOAD_CHANNEL': 0,
-                }
+    'SIGNUP_ENABLED': False,
+    'SIGNUP_CHANNEL': 0,
+    'WELCOME_MESSAGE': 'Welcome to guild server. Use /signup command if you want to join our guild!',
+    'EPGP_ENABLED': False,
+    'EPGP_UPLOAD_CHANNEL': 0,
+}
