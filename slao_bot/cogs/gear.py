f81e71893b3f5f1a430e51114082bf439c16f79c
drcrazy <hcb@wowhcb.ru>
2024-08-06 01:55:51 +0300

    Check off-spec loot in raid

--- slao_bot/cogs/gear.py
+++ slao_bot/cogs/gear.py
@@ -1,6 +1,8 @@
+import sqlite3
 from collections import defaultdict
 from dataclasses import dataclass, field
 from itertools import chain
+from sqlite3 import Connection, Cursor
 from typing import Any, Dict, Iterable, List, Optional, Set
 
 import discord
@@ -8,6 +10,7 @@ import tenacity
 from discord import Colour, Embed, app_commands
 from discord.ext import commands
 from utils import enchants
+from utils.config import guild_config
 from utils.constants import SLOT_NAMES, Role
 from utils.models import Raider
 from utils.report import Report
@@ -23,12 +26,14 @@ class RaidWeakEquipment:
     low_quality_gems: Dict[str, Set] = field(default_factory=defaultdict)
     no_enchants: Dict[str, Set] = field(default_factory=defaultdict)
     low_enchants: Dict[str, Set] = field(default_factory=defaultdict)
+    offspec_raid: Dict[str, Set] = field(default_factory=defaultdict)
 
-    def from_raiders(self, raiders: Iterable[Raider]) -> 'RaidWeakEquipment':
+    def from_raiders(self, raiders: Iterable[Raider], guild_id: str, raid_time: int) -> 'RaidWeakEquipment':
         self.empty_sockets = defaultdict(set)
         self.low_quality_gems = defaultdict(set)
         self.no_enchants = defaultdict(set)
         self.low_enchants = defaultdict(set)
+        self.offspec_raid = defaultdict(set)
 
         for raider in raiders:
             for item in raider.gear:
@@ -36,6 +41,8 @@ class RaidWeakEquipment:
                     continue
                 self._check_sockets(raider.name, item)
                 self._check_enchants(raider.name, item)
+                if guild_config.has_section(guild_id) and guild_config[guild_id].getboolean('epgp_enabled'):
+                    self._check_offspec(raider.name, item, guild_id, raid_time)
 
         return self
 
@@ -75,6 +82,28 @@ class RaidWeakEquipment:
         elif item['permanentEnchant'] in enchants.BAD_ENCHANTS[item['slot']]:
             self.low_enchants[raider_name].add(SLOT_NAMES.get(item['slot']))
 
+    def _check_offspec(self, raider_name: str, item: Dict, guild_id: str, raid_time: int) -> None:
+        """Checks if items looted for off-spec during month before raid were equipped in raid
+
+        :param raider_name: Raider name
+        :param item:  Equipped item dictionary
+        :param guild_id: Discord Guild ID. Used to query a proper DB
+        :param raid_time: Start time of the raid
+        """
+        db_name = f'./data/{guild_id}.db'
+        connection: Connection = sqlite3.connect(db_name)
+        cursor: Cursor = connection.cursor()
+
+        cursor.execute('''SELECT * FROM Traffic WHERE
+        item_id=? AND target=? AND (gpa - gpb)=0 AND timestamp BETWEEN ? and ? ''',
+                       (item['id'], raider_name, raid_time - 2592000, raid_time),
+                       )
+        if not cursor.fetchone() is None:
+            self.offspec_raid[raider_name].add(SLOT_NAMES.get(item['slot']))
+
+        cursor.close()
+        connection.close()
+
 
 class Gear(commands.Cog):
 
@@ -89,10 +118,10 @@ class Gear(commands.Cog):
         # noinspection PyUnresolvedReferences
         await interaction.response.defer()
 
-        embed = await self.process_interaction(report_id)
+        embed = await self.process_interaction(report_id, str(interaction.guild.id))
         await interaction.edit_original_response(embed=embed)
 
-    async def process_interaction(self, report_id: str) -> Optional[discord.Embed]:
+    async def process_interaction(self, report_id: str, guild_id: str) -> Optional[discord.Embed]:
         async with WCLClient() as client:
             try:
                 rs = await client.get_table_summary(report_id)
@@ -111,7 +140,8 @@ class Gear(commands.Cog):
         raiders = self._make_raiders(rs)
         # Check gems and enchants
         equipment = RaidWeakEquipment()
-        equipment.from_raiders(chain.from_iterable(raiders.values()))
+        raid_time = int(rs['reportData']['report']['startTime'] / 1000)
+        equipment.from_raiders(chain.from_iterable(raiders.values()), guild_id, raid_time)
 
         embed.add_field(
             name='Нет камней',
@@ -133,6 +163,12 @@ class Gear(commands.Cog):
             value=self._print_raiders(equipment.low_enchants) or 'С пивком потянет!',
             inline=False,
         )
+        if guild_config.has_section(guild_id) and guild_config[guild_id].getboolean('epgp_enabled'):
+            embed.add_field(
+                name='Оффспек лут в рейде',
+                value=self._print_raiders(equipment.offspec_raid) or 'Все молодцы!',
+                inline=False
+            )
 
         return embed
 
